2ac630e3cb2f30cc4bfaf89b09cb29f8103b27f3
Liam Riddell <web@liamriddell.co.uk>
2021-10-17 14:58:52 +0100

    Added Array Support for word field in the json

--- plainly/linter.js
+++ plainly/linter.js
@@ -32,9 +32,9 @@ const Linter = {
     lintProblematic(editor) {
         // Lint the document against the problematic wordlist
         // We also provide a callback that's called when we're generating the hover markdown
-        return this._lintDocumentAgainstWordlist(editor.document, WORDLIST_PROBLEMATIC, (wordDefinition, markdownTemplate) => {
+        return this._lintDocumentAgainstWordlist(editor.document, WORDLIST_PROBLEMATIC, (word, wordDefinition, markdownTemplate) => {
             // Title
-            const capitalizedWord = Utils.capitaliseFirstLetters(wordDefinition.word);
+            const capitalizedWord = Utils.capitaliseFirstLetters(word);
             markdownTemplate.appendMarkdown(`**${capitalizedWord}**`);
             markdownTemplate.appendText("\n");
 
@@ -54,7 +54,7 @@ const Linter = {
 
             // Alternatives
             if (wordDefinition.replacements && wordDefinition.replacements.length > 0) {
-                markdownTemplate.appendMarkdown(`Alternatives: `);
+                markdownTemplate.appendMarkdown(`Replacement(s): `);
                 for (let i = 0; i < wordDefinition.replacements.length; i++) {
                     const word = wordDefinition.replacements[i];
 
@@ -99,32 +99,46 @@ const Linter = {
         for (let wordDefinitionIndex = 0; wordDefinitionIndex < wordlist.length; wordDefinitionIndex++) {
             const wordDefinition = wordlist[wordDefinitionIndex];
 
-            // Generate a regex pattern for this word
-            // 1. Global Search = g
-            // 2. Ignore Case = i
-            // 3. Generate indices for substring matches = d
-            const searchWordPattern = isExperimentalSearchEnabled ? wordDefinition.word.split("").join("(?: |_|-|)") : wordDefinition.word;
-            const searchWordRegex = new RegExp(`${searchWordPattern}`, 'gid');
-
             // Search the document for the dynamic regex
-            const searchResults = [...documentText.matchAll(searchWordRegex)];
+            const searchResults = [];
+
+            // Check if the word is an array or single string 
+            const isArrayOfWords = wordDefinition.word instanceof Array;
+
+            if (isArrayOfWords) {
+                for (let wordIndex = 0; wordIndex < wordDefinition.word.length; wordIndex++) {
+                    const word = wordDefinition.word[wordIndex];
+
+                    // Generate a regex pattern for this word
+                    // 1. Global Search = g
+                    // 2. Ignore Case = i
+                    // 3. Generate indices for substring matches = d
+                    const searchWordPattern = isExperimentalSearchEnabled ? word.split("").join("(?: |_|-|)") : word;
+                    const searchWordRegex = new RegExp(`${searchWordPattern}`, 'gid');
+                    searchResults.push(...documentText.matchAll(searchWordRegex));
+                }
+            } else {
+                const searchWordPattern = isExperimentalSearchEnabled ? wordDefinition.word.split("").join("(?: |_|-|)") : wordDefinition.word;
+                const searchWordRegex = new RegExp(`${searchWordPattern}`, 'gid');
+                searchResults.push(...documentText.matchAll(searchWordRegex));
+            }
 
             // If we have no results then check the next word
             if (!searchResults || searchResults.length <= 0)
                 continue;
 
-            // Optimization: Generate hover message for word (once)  
-            const markdownTemplateCached = this._cachedHoverPreview.get(wordDefinition.word);
-            const markdownTemplate = markdownTemplateCached || new vscode.MarkdownString("", false);
 
-            // Optimization: Generate hover message for word (once) but cache it 
-            if (!markdownTemplateCached) {
+            // Optimization: Generate hover message for word (once) 
+            let markdownTemplateCached = !isArrayOfWords ? this._cachedHoverPreview.get(wordDefinition.word) : null;
+            let markdownTemplate = markdownTemplateCached || new vscode.MarkdownString("", false);
 
+            // Optimization: Generate hover message for word (once) but cache it when we have single word
+            if (!markdownTemplateCached && !isArrayOfWords) {
                 // Security: This will sanitize html
                 markdownTemplate.isTrusted = false;
 
                 // Callback for populating the markdown template by reference 
-                hoverMessageCallback(wordDefinition, markdownTemplate);
+                hoverMessageCallback(wordDefinition.word, wordDefinition, markdownTemplate);
 
                 // Cache the hover preview
                 this._cachedHoverPreview.set(wordDefinition.word, markdownTemplate);
@@ -134,11 +148,32 @@ const Linter = {
             for (let searchResultIndex = 0; searchResultIndex < searchResults.length; searchResultIndex++) {
                 const searchResult = searchResults[searchResultIndex];
 
+                // Search result word
+                const word = searchResult[0];
+
                 // Get the line number by counting the number of newline characters (\n) before the character position
                 // Use VS Code to get the line number and character position of the match. Saves us doing the above.
                 const startPos = document.positionAt(searchResult.index);
-                const endPos = document.positionAt(searchResult.index + searchResult[0].length);
+                const endPos = document.positionAt(searchResult.index + word.length);
                 const range = new vscode.Range(startPos, endPos);
+
+                // Optimization: Generate hover message for word (once) but cache it when we have multiple words for definition
+                if (isArrayOfWords) {
+                    // Try and get cached template for this word if it exists
+                    markdownTemplateCached = this._cachedHoverPreview.get(word);
+
+                    // Instantiate mardown template
+                    markdownTemplate = markdownTemplateCached || new vscode.MarkdownString("", false);
+
+                    // Security: This will sanitize html
+                    markdownTemplate.isTrusted = false;
+
+                    // Callback for populating the markdown template by reference 
+                    hoverMessageCallback(word, wordDefinition, markdownTemplate);
+
+                    // Cache the hover preview
+                    this._cachedHoverPreview.set(wordDefinition.word, markdownTemplate);
+                }
 
                 // Push the decoration to an array based on severity levels
                 // Default: Low
